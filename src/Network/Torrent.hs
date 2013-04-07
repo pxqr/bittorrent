7f017c7850198a361f2630432ff050c1555d39f5
Sam T <sta.cs.vsu@gmail.com>
2013-04-07 04:07:00 +0400

    add main module

---
+++ src/Network/Torrent.hs
@@ -0,0 +1,11 @@
+module Network.Torrent
+       ( module Network.Torrent.PeerID
+       , module Network.Torrent.THP
+       , module Network.Torrent.Handshake
+       , module Network.Torrent.PWP
+       ) where
+
+import Network.Torrent.PWP
+import Network.Torrent.THP
+import Network.Torrent.Handshake
+import Network.Torrent.PeerID
