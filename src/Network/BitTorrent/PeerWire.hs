a3b7d8442feaec251c38609712d24aea208becbb
Sam T <sta.cs.vsu@gmail.com>
2013-04-24 22:57:05 +0400

    ~ Export ClientInfo module.

--- src/Network/BitTorrent/PeerWire.hs
+++ src/Network/BitTorrent/PeerWire.hs
@@ -10,8 +10,10 @@ module Network.BitTorrent.PeerWire
        ( module Network.BitTorrent.PeerWire.Block
        , module Network.BitTorrent.PeerWire.Message
        , module Network.BitTorrent.PeerWire.Handshake
+       , module Network.BitTorrent.PeerWire.ClientInfo
        ) where
 
 import Network.BitTorrent.PeerWire.Block
 import Network.BitTorrent.PeerWire.Message
 import Network.BitTorrent.PeerWire.Handshake
+import Network.BitTorrent.PeerWire.ClientInfo
