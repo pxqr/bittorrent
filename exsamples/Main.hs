eba89394fa87256fd2ec71b67e667032e9e765dd
Sam T <pxqr.sta@gmail.com>
2013-06-11 05:01:40 +0400

    ~ Add exsamples.

---
+++ exsamples/Main.hs
@@ -0,0 +1,20 @@
+module Main (main) where
+
+import Data.Bitfield
+import Network.BitTorrent
+import System.Environment
+
+
+main :: IO ()
+main = do
+  [path] <- getArgs
+  Right torrent <- fromFile path
+
+  client  <- newClient []
+  swarm   <- newLeacher  client torrent
+
+  discover swarm $ \se -> do
+    peers <- getPeerList se
+    print peers
+
+  print "Bye-bye!"
