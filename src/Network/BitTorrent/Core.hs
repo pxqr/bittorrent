6eefd65b82bb277d84473d39618ca18a8cfe7152
Sam Truzjan <pxqr.sta@gmail.com>
2013-12-05 02:22:14 +0400

    Add Network.BitTorrent.Core module

--- src/Network/BitTorrent/Core.hs
+++ src/Network/BitTorrent/Core.hs
@@ -1,1 +1,12 @@
-module Network.BitTorrent.Core () where
+-- |
+--   Copyright   :  (c) Sam Truzjan 2013
+--   License     :  BSD3
+--   Maintainer  :  pxqr.sta@gmail.com
+--   Stability   :  experimental
+--   Portability :  portable
+--
+--   Re-export every @Network.BitTorrent.Core.*@ module.
+--
+module Network.BitTorrent.Core (module Core) where
+import Network.BitTorrent.Core.PeerId   as Core
+import Network.BitTorrent.Core.PeerAddr as Core
