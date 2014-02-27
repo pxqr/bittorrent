024c65c4f626018925a8aa158c9bf1a3a82ef843
Sam Truzjan <pxqr.sta@gmail.com>
2014-02-27 16:01:49 +0400

    Fix indentation

--- tests/Network/BitTorrent/Tracker/RPC/HTTPSpec.hs
+++ tests/Network/BitTorrent/Tracker/RPC/HTTPSpec.hs
@@ -23,30 +23,31 @@ validateInfo AnnounceQuery {..}  AnnounceInfo {..} = do
 
 spec :: Spec
 spec = parallel $ do
-  forM_ (L.filter isHttpTracker trackers) $ \ TrackerEntry {..} ->
-    context trackerName $ do
+  describe "RPC" $ do
+    forM_ (L.filter isHttpTracker trackers) $ \ TrackerEntry {..} ->
+      context trackerName $ do
 
-      describe "announce" $ do
-        if tryAnnounce
-          then do
-            it "have valid response" $ do
-              withManager def $ \ mgr -> do
---                q    <- arbitrarySample
-                let q = AnnounceQuery def "-HS0003-203534.37420" 6000
-                        (Progress 0 0 0) Nothing Nothing (Just Started)
-                info <- announce mgr trackerURI q
-                validateInfo q info
-          else do
-            it "should fail with RequestFailed" $ do
-               pending
+        describe "announce" $ do
+          if tryAnnounce
+            then do
+              it "have valid response" $ do
+                withManager def $ \ mgr -> do
+--                  q    <- arbitrarySample
+                  let q = AnnounceQuery def "-HS0003-203534.37420" 6000
+                          (Progress 0 0 0) Nothing Nothing (Just Started)
+                  info <- announce mgr trackerURI q
+                  validateInfo q info
+            else do
+              it "should fail with RequestFailed" $ do
+                 pending
 
-      describe "scrape" $ do
-        if tryScraping
-          then do
-            it "have valid response" $ do
-              withManager def $ \ mgr -> do
-                xs <- scrape mgr trackerURI [def]
-                L.length xs `shouldSatisfy` (>= 1)
-          else do
-            it "should fail with ScrapelessTracker" $ do
-              pending
+        describe "scrape" $ do
+          if tryScraping
+            then do
+              it "have valid response" $ do
+                withManager def $ \ mgr -> do
+                  xs <- scrape mgr trackerURI [def]
+                  L.length xs `shouldSatisfy` (>= 1)
+            else do
+              it "should fail with ScrapelessTracker" $ do
+                pending
