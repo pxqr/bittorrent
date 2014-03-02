ba37e451319c94ecc14b2a58a39250a27353f75f
Sam Truzjan <pxqr.sta@gmail.com>
2014-03-02 16:22:32 +0400

    Add todo

--- src/Network/BitTorrent/DHT.hs
+++ src/Network/BitTorrent/DHT.hs
@@ -136,6 +136,7 @@ bootstrap startNodes = do
   $(logInfoS) "bootstrap" "Start node bootstrapping"
   nid <- asks thisNodeId
   -- TODO filter duplicated in startNodes list
+  -- TODO retransmissions for startNodes
   aliveNodes <- queryParallel (pingQ <$> startNodes)
   _ <- sourceList [aliveNodes] $= search nid (findNodeQ nid) $$ C.consume
   $(logInfoS) "bootstrap" "Node bootstrapping finished"
