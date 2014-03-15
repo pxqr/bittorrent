57c07536dd59595ec4cbcce24f0e6352e99a82c6
Sam Truzjan <pxqr.sta@gmail.com>
2014-03-15 19:02:23 +0400

    Add exchange session spec stubs

--- tests/Network/BitTorrent/Exchange/SessionSpec.hs
+++ tests/Network/BitTorrent/Exchange/SessionSpec.hs
@@ -12,25 +12,53 @@ import Config
 nullLogger :: LogFun
 nullLogger _ _ x _ = print x
 
+simpleSession :: InfoDict -> (Session -> IO ()) -> IO ()
+simpleSession dict action = do
+  withRemoteAddr $ \ addr -> do
+    myAddr <- getMyAddr
+    ses  <- newSession nullLogger myAddr "" dict
+    connect addr ses
+    action ses
+    closeSession ses
+
 spec :: Spec
 spec = do
-  describe "metadata exchange" $ do
-    it "should fetch info dictionary" $ do
-      withRemoteAddr $ \ addr -> do
+  describe "construction" $ do
+    describe "newSession" $ do
+      it "" $ do
+        pending
+
+    describe "closeSession" $ do
+      it "" $ do
+        pending
+
+  describe "connection set" $ do
+    describe "connect" $ do
+      it "" $ do
+        pending
+
+    describe "establish" $ do
+      it "" $ do
+        pending
+
+  describe "exchange" $ do
+    describe "metadata" $ do
+      it "should fetch info dictionary" $ do
         Torrent {..} <- getTestTorrent
-        myAddr <- getMyAddr
-        ses  <- newSession nullLogger myAddr "" tInfoDict
-        connect addr ses
-        dict <- waitMetadata ses
-        closeSession ses
-        dict `shouldBe` tInfoDict
+        simpleSession tInfoDict $ \ ses -> do
+          dict <- waitMetadata ses
+          dict `shouldBe` tInfoDict
 
-    it "should serve info dictionary" $ do
-      pending
+      it "should serve info dictionary" $ do
+        pending
 
-  describe "content exchange" $ do
-    it "should fetch torrent content" $ do
-      pending
+    describe "content" $ do
+      it "should fetch torrent content" $ do
+        Torrent {..} <- getTestTorrent
+        simpleSession tInfoDict $ \ ses -> do
+          pending
+--        st <- waitData ses
+--        verifyStorage st (idPieceInfo tInfoDict)
 
-    it "should serve torrent content" $ do
-      pending
+      it "should serve torrent content" $ do
+        pending
