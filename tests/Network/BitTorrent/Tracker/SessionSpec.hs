70a172a43d5b34853289bbf57c3e5a168431a954
Sam Truzjan <pxqr.sta@gmail.com>
2014-03-18 00:18:00 +0400

    Spec `getStatus` and notify `dependency`

--- tests/Network/BitTorrent/Tracker/SessionSpec.hs
+++ tests/Network/BitTorrent/Tracker/SessionSpec.hs
@@ -1,7 +1,6 @@
 module Network.BitTorrent.Tracker.SessionSpec (spec) where
 import Data.Default
 import Data.List as L
-import Network.URI
 import Test.Hspec
 
 import Data.Torrent
@@ -10,15 +9,36 @@ import Network.BitTorrent.Tracker.List
 import Network.BitTorrent.Tracker.RPC
 import Network.BitTorrent.Tracker.Session
 
-import Network.BitTorrent.Tracker.TestData
+import Config
 
+testSession :: (Manager -> Session -> IO ()) -> IO ()
+testSession action = do
+  t <- getTestTorrent
+  withManager def def $ \ m ->
+    withSession (idInfoHash (tInfoDict t)) (trackerList t) $ \ s ->
+      action m s
 
 spec :: Spec
 spec = do
   describe "Session" $ do
-    it "" $ do
-      withManager def def $ \ m -> do
-        s <- newSession def undefined
+    it "start new session in paused state" $ do
+      testSession $ \ _ s -> do
+        status <- getStatus s
+        status `shouldBe` Paused
+
+  describe "Query" $ do
+    it "change status after notify" $ do
+      testSession $ \ m s -> do
+        notify m s Started
+        status <- getStatus s
+        status `shouldBe` Running
+
+        notify m s Stopped
+        stopped <- getStatus s
+        stopped `shouldBe` Paused
+
+    it "return non-empty list of peers" $ do
+      testSession $ \ m s -> do
         notify m s Started
         peers <- askPeers m s
         peers `shouldSatisfy` (not . L.null)
