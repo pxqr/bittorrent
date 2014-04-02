35a049358dbc84f1b24e8e6d3f7e5e1dc0d5e0ae
Sam Truzjan <pxqr.sta@gmail.com>
2014-04-03 00:58:59 +0400

    [Client] Add spec item for openMagnet

---
+++ tests/Network/BitTorrent/Client/HandleSpec.hs
@@ -0,0 +1,19 @@
+module Network.BitTorrent.Client.HandleSpec (spec) where
+import Data.Default
+import Test.Hspec
+
+import Data.Torrent.Magnet
+import Network.BitTorrent.Client
+import Network.BitTorrent.Client.Handle
+
+data_dir :: FilePath
+data_dir = "data"
+
+spec :: Spec
+spec = do
+  describe "openMagnet" $ do
+    it "should add new infohash to index" $ do
+      simpleClient $ do
+        _ <- openMagnet data_dir (nullMagnet def)
+        _ <- getHandle def
+        return ()
